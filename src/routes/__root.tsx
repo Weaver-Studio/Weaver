ab18ace9b248e1a388f3c75e1cf16e6b02eb7df5
solomon Rajkumar <solomonrajkumar43@gmail.com>
2025-10-10 15:52:23 +0530

    init tanstack on convex or better auth

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,103 +1,48 @@
 /// <reference types="vite/client" />
+import type { ReactNode } from 'react'
 import {
-  Outlet,
-  createRootRouteWithContext,
-  useRouteContext,
-  HeadContent,
-  Scripts,
+	Outlet,
+	createRootRoute,
+	HeadContent,
+	Scripts,
 } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
-import { QueryClient } from '@tanstack/react-query'
-import * as React from 'react'
-import appCss from '@/styles/app.css?url'
-import { ConvexBetterAuthProvider } from '@convex-dev/better-auth/react'
-import { authClient } from '@/lib/auth-client'
-import {
-  fetchSession,
-  getCookieName,
-} from '@convex-dev/better-auth/react-start'
-import { getCookie, getRequest } from '@tanstack/react-start/server'
-import { seo } from '@/utils/seo'
-import { ConvexQueryClient } from '@convex-dev/react-query'
-import { createServerFn } from '@tanstack/react-start'
 
-// Get auth information for SSR using available cookies
-const fetchAuth = createServerFn({ method: 'GET' }).handler(async () => {
-  const { createAuth } = await import('@convex/auth')
-  const { session } = await fetchSession(getRequest())
-  const sessionCookieName = getCookieName(createAuth)
-  const token = getCookie(sessionCookieName)
-  return {
-    userId: session?.user.id,
-    token,
-  }
-})
-
-export const Route = createRootRouteWithContext<{
-  queryClient: QueryClient
-  convexQueryClient: ConvexQueryClient
-}>()({
-  head: () => ({
-    meta: [
-      {
-        charSet: 'utf-8',
-      },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1',
-      },
-      ...seo({
-        title: 'Convex + Better Auth + TanStack Start',
-        description: `Convex + Better Auth + TanStack Start`,
-      }),
-    ],
-    links: [
-      { rel: 'stylesheet', href: appCss },
-      { rel: 'icon', href: '/favicon.ico' },
-    ],
-  }),
-  beforeLoad: async (ctx) => {
-    const { userId, token } = await fetchAuth()
-
-    // During SSR only (the only time serverHttpClient exists),
-    // set the auth token to make HTTP queries with.
-    if (token) {
-      ctx.context.convexQueryClient.serverHttpClient?.setAuth(token)
-    }
-
-    return {
-      userId,
-      token,
-    }
-  },
-  component: RootComponent,
+export const Route = createRootRoute({
+	head: () => ({
+		meta: [
+			{
+				charSet: 'utf-8',
+			},
+			{
+				name: 'viewport',
+				content: 'width=device-width, initial-scale=1',
+			},
+			{
+				title: 'TanStack Start Starter',
+			},
+		],
+	}),
+	component: RootComponent,
 })
 
 function RootComponent() {
-  const context = useRouteContext({ from: Route.id })
-  return (
-    <ConvexBetterAuthProvider
-      client={context.convexQueryClient.convexClient}
-      authClient={authClient}
-    >
-      <RootDocument>
-        <Outlet />
-      </RootDocument>
-    </ConvexBetterAuthProvider>
-  )
+	return (
+		<RootDocument>
+			<Outlet />
+		</RootDocument>
+	)
 }
 
-function RootDocument({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en" className="dark">
-      <head>
-        <HeadContent />
-      </head>
-      <body className="bg-neutral-950 text-neutral-50">
-        {children}
-        <TanStackRouterDevtools position="bottom-right" />
-        <Scripts />
-      </body>
-    </html>
-  )
+function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
+	return (
+		<html>
+			<head>
+				<HeadContent />
+			</head>
+			<body>
+				{children}
+				<Scripts />
+			</body>
+		</html>
+	)
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,18 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import tsConfigPaths from 'vite-tsconfig-paths'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
-  plugins: [
-    tsconfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
-    // tailwindcss(), sentry(), ...
-    tanstackStart(),
-    viteReact(),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+	server: {
+		host: "test",
+		port: 5170,
+	},
+	plugins: [
+		tsConfigPaths(),
+		tanstackStart(),
+		// react's vite plugin must come after start's vite plugin
+		viteReact(),
+	],
 })
