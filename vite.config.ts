2989527a03083bb3925e4bfba9b95562543c53e1
solomon Rajkumar <solomonrajkumar43@gmail.com>
2025-10-10 16:08:01 +0530

    added sub git module and shad cn

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
 	server: {
@@ -15,4 +16,10 @@ export default defineConfig({
 		// react's vite plugin must come after start's vite plugin
 		viteReact(),
 	],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+			'@convex': path.resolve(__dirname, './convex/convex'),
+		},
+	},
 })
