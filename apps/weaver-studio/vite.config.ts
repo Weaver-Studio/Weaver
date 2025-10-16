d02d566469073813f769a164b62bd71c26750cb9
Cute Appi <cuteappis@gmail.com>
2025-10-16 20:51:57 +0530

    got website working

--- apps/weaver-studio/vite.config.ts
+++ apps/weaver-studio/vite.config.ts
@@ -4,30 +4,34 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import path from "path"
 import viteReact from '@vitejs/plugin-react';
 
+
 // https://vite.dev/config/
-export default defineConfig({
-	server: ({
-		allowedHosts: ['.test.com'],
-		host: 'app.test.com',
-		port: 5190,
-	}),
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-			'@convex': path.resolve(__dirname, './convex/convex'),
-		},
-	},
-	plugins: [
-		tsConfigPaths(),
-		tanstackStart(),
-		viteReact({
-			babel: {
-				plugins: [
-					['babel-plugin-react-compiler', { /* optional configuration, e.g., target: '18' for React 18 */ }],
-				],
-			},
+export default defineConfig(() => {
+	return {
+		server: ({
+			allowedHosts: ['.test.com'],
+			host: 'app.test.com',
+			port: 5190,
 		}),
+		resolve: {
+			alias: {
+				'@': path.resolve(__dirname, './src'),
+				'@convex': path.resolve(__dirname, './convex/convex'),
+			},
+		},
+		envDir: path.resolve(__dirname, '../../'),
+		plugins: [
+			tsConfigPaths(),
+			tanstackStart(),
+			viteReact({
+				babel: {
+					plugins: [
+						['babel-plugin-react-compiler', { /* optional configuration, e.g., target: '18' for React 18 */ }],
+					],
+				},
+			}),
 
-	],
+		],
 
+	}
 });
--- apps/weaver-website/src/routes/__root.tsx
+++ apps/weaver-website/src/routes/__root.tsx
@@ -20,7 +20,7 @@ import { authClient } from "@/lib/auth-client";
 import appCss from '@/styles/app.css?url'
 
 const fetchAuth = createServerFn({ method: 'GET' }).handler(async () => {
-	const { createAuth } = await import('@convex/auth')
+	const { createAuth } = await import('@weaver/backend')
 	const { session } = await fetchSession(getRequest())
 	const sessionCookieName = getCookieName(createAuth)
 	const token = getCookie(sessionCookieName)
