9411e31d0cff395e2bf0b7f802019516e293e2af
Cute Appi <cuteappis@gmail.com>
2025-10-20 12:12:59 +0530

    fixed tsconfig linting error

--- apps/website/vite.config.ts
+++ apps/website/vite.config.ts
@@ -1,30 +1,36 @@
 // vite.config.ts
-import { defineConfig } from 'vite'
-import tsConfigPaths from 'vite-tsconfig-paths'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
-import path from 'path'
+
+import path from "node:path";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	server: {
-		host: "test.com",
-		port: 5170,
-	},
-	plugins: [
-		tsConfigPaths(),
-		tanstackStart(),
-		// react's vite plugin must come after start's vite plugin
-		viteReact({
-			babel: {
-				plugins: [
-					['babel-plugin-react-compiler', { /* optional configuration, e.g., target: '18' for React 18 */ }],
-				],
-			},
-		}),
-	],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-		},
-	},
-})
+  server: {
+    host: "test.com",
+    port: 5170,
+  },
+  plugins: [
+    tsConfigPaths(),
+    tanstackStart(),
+    // react's vite plugin must come after start's vite plugin
+    viteReact({
+      babel: {
+        plugins: [
+          [
+            "babel-plugin-react-compiler",
+            {
+              /* optional configuration, e.g., target: '18' for React 18 */
+            },
+          ],
+        ],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
